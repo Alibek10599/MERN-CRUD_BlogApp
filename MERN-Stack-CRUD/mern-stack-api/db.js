9e44cbd159dea695ae298371004503372f7ccbef
Alibek10599 <59087203+Alibek10599@users.noreply.github.com>
2021-02-06 18:05:24 +0600

    Update db.js

--- MERN-Stack-CRUD/mern-stack-api/db.js
+++ MERN-Stack-CRUD/mern-stack-api/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://alibek9999:1234qwerty@cluster0.n2h84.mongodb.net/test2?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',{useNewUrlParser:true,useUnifiedTopology:true},
+mongoose.connect('Ur URL',{useNewUrlParser:true,useUnifiedTopology:true},
     err => {
         if (!err)
             console.log('Mongodb connection succeeded.')
